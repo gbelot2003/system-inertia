f387d093828d8cd29c54a4b1e78fa80bb0d59575
Gerardo Belot <gerardo.belot@gmail.com>
2022-12-20 18:06:13 -0600

    Cambiando a layout compartido

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+import { Inertia } from '@inertiajs/inertia';
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+const logout = () => {
+    Inertia.post(route('logout'));
+};
+</script>
+<template>
+    <main>
+        <slot />
+    </main>
+</template>
